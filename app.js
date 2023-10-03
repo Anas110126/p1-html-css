97855ee7d0e8d48cd81f767a6c2996e55fd3db2b
Anas110126 <145332024+Anas110126@users.noreply.github.com>
2023-10-03 13:27:08 +0200

    Add files via upload
    opdracht 6.1

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementById("titles").style.color = "blue"
+document.getElementById("titles").style.fontFamily = "arial"
+function changeColor()
+{
+
+ document.getElementById("titles").style.color = "blue";
+ console.log("de functie werkt");
+
+}
